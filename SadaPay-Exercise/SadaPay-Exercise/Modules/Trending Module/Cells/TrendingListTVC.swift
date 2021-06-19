c828fa90eca19c8c179bfdba7d4fab1c87dbc8f6
Saifullah Fazlani <saif.fazlani2906@gmail.com>
2021-06-19 21:02:46 +0500

    Modified table view cells.
    Also added extension for UIView.

--- SadaPay-Exercise/SadaPay-Exercise/Modules/Trending Module/Cells/TrendingListTVC.swift
+++ SadaPay-Exercise/SadaPay-Exercise/Modules/Trending Module/Cells/TrendingListTVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SDWebImage
 
 class TrendingListTVC: UITableViewCell {
 
@@ -14,16 +15,20 @@ class TrendingListTVC: UITableViewCell {
     @IBOutlet weak var lblName: UILabel!
     @IBOutlet weak var lblFullName: UILabel!
     @IBOutlet weak var lblDescription: UILabel!
+    @IBOutlet weak var blueDot: UIView!
     @IBOutlet weak var lblLanguage: UILabel!
     @IBOutlet weak var lblRating: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        avatarImageView.makeRounded()
+        avatarImageView.setBorders(width: 1.0, color: .systemGreen)
+        blueDot.makeRounded()
     }
     
     func setData(object: Item?) {
         guard let repoItem = object, let avatarUrl = repoItem.owner?.avatarUrl else {return}
-        avatarImageView.image = UIImage(named: avatarUrl)
+        avatarImageView.sd_setImage(with: URL(string: avatarUrl), placeholderImage: #imageLiteral(resourceName: "placeholderImage"))
         lblName.text = repoItem.name
         lblFullName.text = repoItem.fullName
         lblDescription.text = repoItem.itemDescription
